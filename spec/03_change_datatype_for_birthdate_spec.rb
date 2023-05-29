6ae19cf008b00b33a4549098084e3499d3e98d62
cliff <cliffmudigo5@gmail.com>
2023-05-29 23:28:40 +0300

    partially done

--- spec/03_change_datatype_for_birthdate_spec.rb
+++ spec/03_change_datatype_for_birthdate_spec.rb
@@ -2,12 +2,12 @@ require_relative '../db/migrate/01_create_students'
 require_relative '../db/migrate/02_add_grade_and_birthdate_to_students'
 require_relative '../db/migrate/03_change_datatype_for_birthdate'
 
-describe ChangeDatatypeForBirthdate do
+describe AddGradeAndBirthdateToStudents do
   before do
     sql = "DROP TABLE IF EXISTS students"
     ActiveRecord::Base.connection.execute(sql)
 
-    # Manually runs the migration
+    # Manually runs the migrations
     CreateStudents.new.change
     AddGradeAndBirthdateToStudents.new.change
     ChangeDatatypeForBirthdate.new.change
@@ -15,8 +15,9 @@ describe ChangeDatatypeForBirthdate do
     Student.reset_column_information
   end
 
-  it 'has a birthday as a datetime datatype' do
-    student = Student.create(name: "Steven", birthdate: DateTime.new(2000, 1, 1))
-    expect(Student.where(birthdate: DateTime.new(2000, 1, 1)).first).to eq(student)
+  it 'has a string birthdate' do
+    birthdate = Date.parse("April 5th")
+    student = Student.create(name: "Steven", grade: 12, birthdate: birthdate)
+    expect(Student.where(birthdate: birthdate).first).to eq(student)
   end
 end
